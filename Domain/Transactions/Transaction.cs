8ee1723347bb9af19edd48d62f0b34fa57e06297
devtiagom <tiago.o.machado@gmail.com>
2023-11-22 22:11:26 -0300

    Incluidos pasta domain e entidade Transaction.

---
+++ Domain/Transactions/Transaction.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalFinances.Domain.Transactions
+{
+    public abstract class Transaction
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Payed { get; set; }
+    }
+}
